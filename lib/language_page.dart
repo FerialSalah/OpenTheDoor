b13e47d463746de8e43a7677ae0acec14f8629fa
FerialSalah <ferialsalah177@gmail.com>
2020-01-24 22:34:38 +0200

    add notification page

---
+++ lib/language_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'localization.dart';
+class LanguageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).translateString('language')),
+        backgroundColor: Color(0xFFC89C17),
+        centerTitle: true,
+      ),
+    );
+  }
+}
